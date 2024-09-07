33364373611709c2529e1942ed0763d824858b8b
sr-akash <43218055+SR-Akash@users.noreply.github.com>
2024-09-08 00:44:57 +0600

    first

---
+++ MGM_Lite/DBContext/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=AKASH-300174;Initial Catalog=CRUD_DB;Connect Timeout=30;Encrypt=False;Trusted_Connection=true;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+
+
+}
